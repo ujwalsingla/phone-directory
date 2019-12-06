04c08fc05b14e92294049a68334a289891fe68b3
ujwalsingla <ujwalsingla2577@gmail.com>
2019-12-06 23:33:03 +0530

    Smart & Dumb: Added ‘PhoneDirectory’ as smart component, renamed ‘App…

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -19,6 +19,12 @@ class AddSusbscriber extends Component {
         this.setState(state);
     }
 
+    onFormSubmitted = (e) => {
+        e.preventDefault();
+        this.props.addSubscriberHandler(this.state);
+        this.setState({ id: 0, name: '', phone: ' ' });
+    }
+
     render() {
         const {name, phone} = this.state;
         return (
@@ -27,7 +33,7 @@ class AddSusbscriber extends Component {
                 <div className="component-body-container">
                     <button className="custom-btn">Back</button>
 
-                    <form className="subscriber-form">
+                    <form className="subscriber-form" onSubmit={this.onFormSubmitted.bind(this)}>
                         <label htmlFor="name" className="label-control">Name: </label><br/>
                         <input id="name" type="text" className="input-control" name="name" onChange={this.inputChangedHandler}/><br/><br/>
                         <label htmlFor="phone" className="label-control">Phone: </label><br/>
--- src/ShowSubscribers.js
+++ src/ShowSubscribers.js
@@ -1,13 +1,7 @@
 import React, {Component} from 'react';
 import Header from './Header.js';
-import './App.css';
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      subscribersListToShow: []
-    }
-  }
+import './ShowSubscribers.css';
+class ShowSubscribers extends Component {
   render(){
 
   return (
@@ -21,7 +15,7 @@ class App extends Component {
              <span className="grid-item phone-heading">Phone</span>
              </div>
              {
-             this.state.subscribersListToShow.map(sub => {
+             this.props.subscribersList.map(sub => {
                return <div key={sub.id} className="grid-container">
                  <span className="grid-item">{sub.name}</span>
                  <span className="grid-item">{sub.phone}</span>
@@ -37,4 +31,4 @@ class App extends Component {
 }
 }
 
-export default App;
+export default ShowSubscribers;
