0455033ff018e0bd74f1cab1618b6f9b56ca2bc7
ujwalsingla <ujwalsingla2577@gmail.com>
2019-12-10 15:15:22 +0530

    Updates

---
+++ precache-manifest.4fef483c7491c883b8e1af8989dd9b58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589e69e1bd8dd7c694e7b8b7aebbb63a",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "103dcddc6befea75031d",
+    "url": "/phone-directory/static/css/main.e9538e35.chunk.css"
+  },
+  {
+    "revision": "ad88fc23cbbb8365a20c",
+    "url": "/phone-directory/static/js/2.0e4944c0.chunk.js"
+  },
+  {
+    "revision": "103dcddc6befea75031d",
+    "url": "/phone-directory/static/js/main.6d28048e.chunk.js"
+  },
+  {
+    "revision": "123152631e28e7abc5f0",
+    "url": "/phone-directory/static/js/runtime-main.337be516.js"
+  }
+]);
